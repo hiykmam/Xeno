277fdfa1d7f3b55cb0ccba01b6cfce38cf7e80fa
hiykmam <kaeri.hina@gmail.com>
2026-02-27 18:34:36 +0900

    テスト

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,16 +1,28 @@
+import os 
+import json
+
+from dotenv import load_dotenv
 from typing import TYPE_CHECKING
 from sanic import Sanic, response, Request, Websocket
-from util.webSoketManager import WebScketAllRoundManager as WSARM
 
-import json
+from util.webSoketManager import WebScketAllRoundManager as WSARM
+from util.OAuth2Manager import DiscordCertificationManager as DCM
+
+
 
 if TYPE_CHECKING:
     from game.card import _CardBase
     from game.table import Table
     from game.table import Deck
 
-app = Sanic("XenoApp")
+env = load_dotenv(".env")
 ws_manager = WSARM()
+oa_manager = DCM(
+    client_id=os.getenv("DISCORD_CLIENT_ID"),
+    client_secret=os.getenv("DISCORD_CLIENT_SECRET"),
+    redirect_uri=os.getenv("DISCORD_REDIRECT_URI")
+)
+app = Sanic("XenoApp")
 
 # プロキシ経由で届くリクエストを処理
 @app.get("/api/status")
@@ -31,27 +43,7 @@ async def get_websocet(request: Request, ws: Websocket):
 
     try:
         while True:
-            raw_data = await ws.recv()
-            data = json.loads(raw_data)
-
-            # 例: 試合が決着した時
-            if data.get("type") == "game_over" and mode == "player":
-                # winner_idを受け取る想定
-                winner_id = data.get("winner_id")
-                
-                # DB更新ロジック (Tortoise)
-                # me = await user.get(user_id=user_id)
-                # if user_id == winner_id: await me.win()
-                # else: await me.lose()
-
-                # 部屋の全員に結果を通知
-                payload = {
-                    "type": "result",
-                    "winner": winner_id,
-                    "new_rate": 1005, # me.rate
-                    "rank": "霊峰"      # me.rank_class
-                }
-                await ws_manager.broadcast(room_id, payload)
+            pass
 
     except Exception:
         pass
